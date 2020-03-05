b1b06197e90f18062f734de2e735467274805ffb
BroccoliRules <bigal6@gmail.com>
2020-03-05 13:22:27 -0700

    First commit

---
+++ tethysapp/about/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class About(TethysAppBase):
+    """
+    Tethys app class for About.
+    """
+
+    name = 'About'
+    index = 'about:home'
+    icon = 'about/images/icon.gif'
+    package = 'about'
+    root_url = 'about'
+    color = '#8e44ad'
+    description = ''
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='about',
+                controller='about.controllers.home'
+            ),
+        )
+
+        return url_maps
